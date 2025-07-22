8feb192c7af08f566c28b1331490c615d9cb5689
Tia Piol <tiarox02@yahoo.com>
2025-07-22 12:16:12 -0700

    sign users out

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,5 +53,10 @@ router.post("/sign-in", async (req, res) => {
     res.redirect("/");
 })
 
+router.get("/sign-out", (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
+});
+
 
 module.exports = router;
